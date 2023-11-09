6cc31d0acbdf18a82fa66a968804458e39d1dae6
lucia <luciafme@gmail.com>
2023-11-09 22:50:44 +0000

    created methods in OpenWeatherMapSupplier

--- src/main/java/practica1_dacd_afonso_medina/control/Main.java
+++ src/main/java/practica1_dacd_afonso_medina/control/Main.java
@@ -3,30 +3,26 @@ package practica1_dacd_afonso_medina.control;
 import practica1_dacd_afonso_medina.FileManager;
 import practica1_dacd_afonso_medina.OpenWeatherMapSupplier;
 import practica1_dacd_afonso_medina.SqLiteWeatherStore;
+import practica1_dacd_afonso_medina.WeatherControler;
 import practica1_dacd_afonso_medina.model.Location;
 
 import java.sql.SQLException;
 import java.util.List;
+import java.util.Timer;
 
 public class Main {
     public static void main(String[] args) throws SQLException {
-        List<Location> listLocation = List.of(
-                new Location(28.12380904158049, -15.436162953343267, "GranCanaria"),
-                new Location(28.463850790803008, -16.25097353346818, "Tenerife"),
-                new Location(28.50047229032077, -13.863339828212446, "Fuerteventura"),
-                new Location(28.965080860301025, -13.556148106209083, "Lanzarote"),
-                new Location(29.23141101200906, -13.503131221117982, "LaGraciosa"),
-                new Location(27.809920552606453, -17.91474223115781, "ElHierro"),
-                new Location(28.094369991798228, -17.109467831251514, "LaGomera"),
-                new Location(28.684160726614596, -17.76582062032028, "LaPalma"));
+
         FileManager fileManager = new FileManager();
         String apikey = fileManager.readFile("C:\\Users\\lucia\\IdeaProjects\\DACD_1aPractica\\src\\main\\resources\\apiKey.txt");
 
+        Timer timer = new Timer();
+        //long period = 6 * 60 * 60 * 1000; // 6 horas en milisegundos
+        long period =  5 * 60 * 1000; // 6 horas en milisegundos
 
-        for(Location location:listLocation) {
-            OpenWeatherMapSupplier openWeatherMapSupplier = new OpenWeatherMapSupplier(apikey);
-            SqLiteWeatherStore sqLiteWeatherStore = new SqLiteWeatherStore("C:\\Users\\lucia\\IdeaProjects\\DACD_1aPractica\\src\\main\\resources\\path.txt");
-            sqLiteWeatherStore.save(openWeatherMapSupplier.getWeather(location));
-        }
+        String path = "C:\\Users\\lucia\\IdeaProjects\\DACD_1aPractica\\src\\main\\resources\\path.txt";
+        WeatherControler weatherControler = new WeatherControler(new OpenWeatherMapSupplier(apikey),new SqLiteWeatherStore(path));
+        timer.schedule(weatherControler,0, period);
+
     }
 }
