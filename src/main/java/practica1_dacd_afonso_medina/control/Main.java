3c16b6e9c932779e7b1bb9e4eecdda68d2ac7097
lucia <luciafme@gmail.com>
2023-11-17 22:10:13 +0000

    finish2 delete extra code

--- src/main/java/practica1_dacd_afonso_medina/control/Main.java
+++ src/main/java/practica1_dacd_afonso_medina/control/Main.java
@@ -1,20 +1,14 @@
 package practica1_dacd_afonso_medina.control;
-
-import practica1_dacd_afonso_medina.control.OpenWeatherMapSupplier;
-import practica1_dacd_afonso_medina.control.SqLiteWeatherStore;
-import practica1_dacd_afonso_medina.control.WeatherControler;
-
 import java.sql.SQLException;
 import java.util.Timer;
 
 public class Main {
     public static void main(String[] args) throws SQLException {
-        String apikey = args[0];
 
         Timer timer = new Timer();
         long period = 6 * 60 * 60 * 1000;
 
-        WeatherControler weatherControler = new WeatherControler(new OpenWeatherMapSupplier(apikey),new SqLiteWeatherStore(args[1]));
+        WeatherControler weatherControler = new WeatherControler(new OpenWeatherMapSupplier(args[0]),new SqLiteWeatherStore(args[1]));
         timer.schedule(weatherControler,0, period);
 
     }
